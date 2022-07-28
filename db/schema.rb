e242daf5d1da8075d547db2a2740523d7b842372
joaolavelino <joaolavelino@gmail.com>
2022-07-27 21:12:56 -0300

    new table artists

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_27_235813) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_28_001151) do
   create_table "albums", force: :cascade do |t|
     t.string "name"
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "label"
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
